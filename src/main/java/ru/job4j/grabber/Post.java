5d29166e3c8652e33ea86df8df83f280adc817b9
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2021-01-27 15:21:31 +0300

    2.2. Модель данных - Post.[285211#336011]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,10 @@
+package ru.job4j.grabber;
+
+import java.util.Calendar;
+
+public class Post {
+    Topic topic;
+    User user;
+    String msg;
+    Calendar date;
+}
---
+++ src/main/java/ru/job4j/grabber/User.java
@@ -0,0 +1,5 @@
+package ru.job4j.grabber;
+
+public class User {
+
+}
