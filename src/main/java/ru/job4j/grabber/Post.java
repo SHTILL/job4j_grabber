ef06e37f4297e2c85f33667542955cab6cffd9de
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2021-01-29 14:12:04 +0300

    2.2. Модель данных - Post.[285211#336011]  2.3. Загрузка деталей поста.[285212#336012]  2.4. SqlRuParse[285213#336013]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -1,10 +1,58 @@
 package ru.job4j.grabber;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 
 public class Post {
-    String   name;
-    String   text;
-    String   link;
-    Calendar created;
+    private String   name;
+    private String   text;
+    private String   link;
+    private Calendar created;
+
+    public Post(String text, Calendar created) {
+        this.text = text;
+        this.created = created;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public Post(String link) {
+        this.link = link;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Calendar getCreated() {
+        return created;
+    }
+
+    public void setCreated(Calendar created) {
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "created=" + DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT).format(created.getTime())  + '\''
+                + ", name='" + name + '\''
+                + ", text='" + text + '\''
+                + ", link='" + link
+                + '}';
+    }
 }
--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -5,31 +5,70 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SqlRuParse implements Parse {
-    public static void main(String[] args) throws Exception {
-        for (int p = 1; p <= 5; p++) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + p).get();
-            Elements topics = doc.select(".postslisttopic");
-            Elements dates = doc.select(".altCol");
-            for (int i = 0, j = 1; i < topics.size(); i++, j += 2) {
-                Element href = topics.get(i).child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                Element date = dates.get(j);
-                System.out.println(date.text());
-            }
-        }
-    }
-
     @Override
-    public List<Post> list(String link) {
-        return null;
+    public List<Post> list(String adsLink) {
+        int page = 1;
+        List<Post> ads = new ArrayList<>();
+
+        try {
+            Document doc = Jsoup.connect(adsLink).get();
+            Elements topics = doc.select(".postslisttopic");
+            for (int i = 3; i < topics.size(); i++) {
+                Element href = topics.get(i).child(0);
+                ads.add(new Post(href.attr("href")));
+            }
+        } catch (IOException e) {
+            System.out.printf("Error while connecting to the link %s" + System.lineSeparator(), adsLink + page);
+            return null;
+        }
+        return ads;
     }
 
     @Override
     public Post detail(String link) {
+        try {
+            Document postDoc = Jsoup.connect(link).get();
+            Elements textElements = postDoc.getElementsByClass("msgBody");
+            String text = textElements.get(1).text();
+            Elements footerElements = postDoc.getElementsByClass("msgFooter");
+            Element createdElement = footerElements.get(0);
+            String created = createdElement.ownText().replaceAll(" \\[] \\|$", "");
+            return new Post(text, DateParser.parseDateString(created));
+        } catch (IOException e) {
+            System.out.printf("Error while connecting to the link %s" + System.lineSeparator(), link);
+        }
         return null;
+    }
+
+    public static void main(String[] args) {
+        Parse parse = new SqlRuParse();
+        List<Post> allPosts = new ArrayList<>();
+
+        int page;
+        final String adsLink = "https://www.sql.ru/forum/job-offers/";
+        for (page = 1; page <= 5; page++) {
+            List<Post> ads = parse.list(adsLink + page);
+            if (ads == null) {
+                return;
+            }
+            for (Post ad: ads) {
+                Post details = parse.detail(ad.getLink());
+                if (details == null) {
+                    return;
+                }
+                ad.setName(details.getName());
+                ad.setCreated(details.getCreated());
+                ad.setText(details.getText());
+            }
+            allPosts.addAll(ads);
+        }
+        allPosts.forEach(System.out::println);
+        System.out.print(allPosts.size());
     }
 }
