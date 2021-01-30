e10ddc783a43d27e82d4ef02ed204229df1d1aba
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2021-01-30 14:52:34 +0300

    4. Схема таблицы Post[1731#336017]

--- db/create_post_table.sql
+++ db/create_post_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE public.post
     id integer NOT NULL,
     link text NOT NULL,
     name text NOT NULL,
-    text text NOT NULL,
+    description text NOT NULL,
     created date NOT NULL,
     CONSTRAINT post_pkey PRIMARY KEY (id),
     CONSTRAINT link_unique UNIQUE (link)
